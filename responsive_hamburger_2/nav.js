32288c11b56c0c6a3a3a3a272c5816a8f5e692ff
Shihab Khan <echoshihab@gmail.com>
2024-01-21 14:36:59 -0500

    complete responsive menu style

--- responsive_hamburger_2/nav.js
+++ responsive_hamburger_2/nav.js
@@ -6,6 +6,7 @@ navToggle.addEventListener("click", () => {
 
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", "true");
+    navToggle.setAttribute("aria-expanded", "true");
   } else {
     primaryNav.setAttribute("data-visible", "false");
     navToggle.setAttribute("aria-expanded", "false");
